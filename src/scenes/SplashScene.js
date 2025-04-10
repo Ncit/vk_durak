2206911181734990b0e7abaf6e5c88fbf1804600
ncit <nikmobdev@gmail.com>
2025-04-10 16:07:24 +0300

    - extra delay

--- src/scenes/SplashScene.js
+++ src/scenes/SplashScene.js
@@ -27,7 +27,7 @@ export class SplashScene extends Phaser.Scene {
         });
 
         this.time.addEvent({
-        delay: 100,
+        delay: 1500,
         loop: false,
         callback: () => {
             start_icon.destroy();
