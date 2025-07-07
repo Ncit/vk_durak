14bfc067dc3ade68dd2d0956f70d722787f165d6
ncit <nikmobdev@gmail.com>
2025-07-07 21:54:19 +0300

    - changed variable

--- src/scripts/vklogic.js
+++ src/scripts/vklogic.js
@@ -1,8 +1,5 @@
-// import { createClient } from '@supabase/supabase-js'
-
 function initVkBridgeApp() {
 	vkBridge.send("VKWebAppInit", {});
-  // const supabase = createClient('https://5e92-2a12-5940-5517-00-2.ngrok-free.app$0', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE')
 }
 
 function friendsInvite() {
@@ -21,7 +18,7 @@ function friendsInvite() {
 }
 
 function setupApp(appDataCallback) {
-  bridge.send('VKWebAppGetLaunchParams')
+  vkBridge.send('VKWebAppGetLaunchParams')
   .then((data) => { 
     if (data.vk_app_id) {
       // Параметры запуска получены
